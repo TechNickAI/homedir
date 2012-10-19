01c17abde55e55dabf85e08b870bcfb85f8f3772
Nick Sullivan <nick@sullivanflock.com>
2012-10-19 09:45:36 -0700

    time limit on curl. abort if there are any errors. wire up .profile to run the auto update on login

--- update.sh
+++ update.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 #### Check to see if we have a new version, if so, offer to install it.
-#### Assumes that the repo is hosted on github
+#### Assumptions:
+# The repo is hosted on github, and public
+# curl is installed
 
 if [ -f ~/.homedir_no_update ] ; then
     exit
 fi
 
+# Abort on error
+set -e
+cd "$(dirname "$0")"
+
 # Pull the repository from git and put sed/awk to work. Kind of a hack, but it works.
 repo=`git remote -v | head -1 | awk '{print $2}' | awk -F ':' '{print $2}' | sed 's/\.git$//'`
 
 local_version=`cat VERSION`
-remote_version=`curl --silent https://raw.github.com/$repo/master/VERSION`
+remote_version=`curl --max-time=3--silent https://raw.github.com/$repo/master/VERSION`
 
 if [ "$local_version" != "$remote_version" ] ; then
     echo "The remote version of $repo is $remote_version, yours is $local_version. Update? (yes/no)"
