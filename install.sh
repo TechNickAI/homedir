ff9fb9f8f68c2ca0e49fdb44ffcaac9903b12af8
Nick Sullivan <nick@sullivanflock.com>
2012-10-19 08:26:56 -0700

    Fix for boolean or with existing files

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Verbose
-set -x
-
 # Abort on error
 set -e
 
 cd ~
 for file in .vimrc .vim .selected_editor .profile .hushlogin ; do
-  if [ -h $file ] ; then 
+  if [ -h $file ] ; then
     # File is already a symbolic link
+    echo "Symlink for $file is already there"
     continue
   fi
-  if [ [ -d $file ] || [ -f $file ] ] ; then 
-    rm -rf $file.b4homedir   
+  if [ -d $file -o -f $file ] ; then 
+    echo "Backing up existing $file to $file.b4homedir"
+    rm -rf $file.b4homedir
     mv $file $file.b4homedir
   fi
+  echo "Creating symlink for $file"
   ln -s homedir/$file
 done
