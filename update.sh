c24dda86251ea839d2539e7af18514811791bac8
Nick Sullivan <nick@sullivanflock.com>
2012-10-19 09:58:23 -0700

    fix for max-time option. Only check for an update every 60 minutes

--- update.sh
+++ update.sh
@@ -5,19 +5,32 @@
 # The repo is hosted on github, and public
 # curl is installed
 
-if [ -f ~/.homedir_no_update ] ; then
+# Change the working directory to the scripts working directory
+cd "$(dirname "$0")"
+
+# Is auto update disabled?
+if [ -f ./.homedir_no_update ] ; then
+    exit
+fi
+
+# Has it been run in the last x minutes?
+last_update=./.homedir_last_update
+if [ "`test -f $last_update && find $last_update -mtime -60`" != "" ] ; then
     exit
 fi
 
 # Abort on error
 set -e
-cd "$(dirname "$0")"
 
 # Pull the repository from git and put sed/awk to work. Kind of a hack, but it works.
 repo=`git remote -v | head -1 | awk '{print $2}' | awk -F ':' '{print $2}' | sed 's/\.git$//'`
 
 local_version=`cat VERSION`
-remote_version=`curl --max-time=3--silent https://raw.github.com/$repo/master/VERSION`
+remote_version=`curl --max-time 3 --silent https://raw.github.com/$repo/master/VERSION`
+# If the curl worked, touch the last update file so we don't check again for a while
+if [ $? -eq 0 ] ; then
+    touch $last_update
+fi
 
 if [ "$local_version" != "$remote_version" ] ; then
     echo "The remote version of $repo is $remote_version, yours is $local_version. Update? (yes/no)"
@@ -25,8 +38,9 @@ if [ "$local_version" != "$remote_version" ] ; then
     if [ "$update" == "yes" ] ; then
         git pull
         ./install.sh
+
     else
         echo "Skipping update for now."
-        echo "If you want to disable this check, then run 'touch ~/.homedir_no_update'"
+        echo "If you want to disable this check, then run 'touch $PWD/.homedir_no_update'"
     fi
 fi
