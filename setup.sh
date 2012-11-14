d77a5c99a7f7a3d5a627ba82c1472d3d58a8a79e
Nick Sullivan <nick@sullivanflock.com>
2012-11-14 15:50:47 -0800

    .jshintrc, passing very strictly

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 set -e
 
 cd ~
-for file in .vimrc .vim .selected_editor .profile .hushlogin ; do
+for file in .vimrc .vim .selected_editor .profile .hushlogin .jshintrc ; do
   if [ -h $file ] ; then
     # File is already a symbolic link
     echo "Symlink for $file is already there"
